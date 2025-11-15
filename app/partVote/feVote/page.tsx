e0b46245dbb2862f8593e58fc4671fb8d5dfc983
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-11-16 03:20:18 +0900

    fix: import 경로 문제 재해결

--- app/partVote/feVote/page.tsx
+++ app/partVote/feVote/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import PartCandidateList from '@/components/partVote/PartCandidateList';
-import { mockPartCandidates } from '@/lib/mockCandidates';
+import { mockPartCandidates } from '@/lib/mockPartCandidates';
 import VoteHeader from '@/components/vote/VoteHeader';
 import SubmitBtn from '@/components/SubmitBtn';
 
