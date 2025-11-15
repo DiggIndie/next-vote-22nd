450bf0ba1ec426399b1aa37842859e212c810fd2
백승선 <baekjimmy@yonsei.ac.kr>
2025-11-15 22:45:23 +0900

    [#7]refactor: FE, BE 투표 애니매이션 수정

--- app/beVoteAnimation/page.tsx
+++ app/beVoteAnimation/page.tsx
@@ -5,7 +5,7 @@ import VoteAnimation from "@/components/voteAnimation";
 
 export default function Vote() {
   return (
-    <main className="h-[315px] w-[169px] flex flex-col bg-[#FFD954] rounded-[19px]">
+    <main className="relative w-full h-screen  w-[375px] flex flex-col bg-[#FFD954]">
       <section className="flex flex-1 flex-col items-center">
         <VoteHeader title={"BE 파트장 투표"} blackDot={2} backBtn={false} />
         <VoteAnimation />
