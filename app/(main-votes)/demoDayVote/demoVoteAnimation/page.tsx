5976ab339d2c8f16fb35405de4af831b79c8e183
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-11-17 00:35:53 +0900

    fix: 폴더 변경 시 발생한 오류 해결

---
+++ app/(main-votes)/demoDayVote/demoVoteAnimation/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import VoteHeader from '@/components/vote/VoteHeader';
+import VoteAnimation from '@/components/vote/VoteAnimation';
+
+export default function DemoVoteAnimationPage() {
+  return (
+    <main className="relative w-full h-screen  w-[375px] flex flex-col bg-[#FFD954]">
+      <section className="flex flex-1 flex-col items-center">
+        <VoteHeader title={'데모데이 투표'} blackDot={2} backBtn={false} />
+        <VoteAnimation />
+      </section>
+    </main>
+  );
+}
---
+++ app/(part-votes)/demoVote/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import VoteHeader from '@/components/vote/VoteHeader';
+import DemoVoteBody from '@/components/demoVote/DemoVoteBody';
+
+export default function DemoVotePage() {
+  return (
+    <main className="relative w-full h-screen  w-[375px] flex flex-col bg-[#FFD954]">
+      <section className="flex flex-1 flex-col items-center">
+        <VoteHeader title={'데모데이 투표'} blackDot={1} backBtn={true} />
+        <DemoVoteBody />
+      </section>
+    </main>
+  );
+}
