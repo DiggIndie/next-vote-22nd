7e4251ca8ad3988ca1695c62a41542f24a71c76e
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-11-17 01:38:57 +0900

    chore: import 방식 변경

--- app/part/beVote/page.tsx
+++ app/part/beVote/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import PartCandidateList from '@/components/partVote/PartCandidateList';
-import { MockPartCandidates } from '@/lib';
+import { MockPartCandidates } from '@/lib/MockPartCandidates';
 import VoteHeader from '@/components/vote/VoteHeader';
 import SubmitButton from '@/components/SubmitButton';
 
